f46e549ae9ae57605119f7581d352c6f78c28688
Rishabh Kumar Jain <rishjain@adobe.com>
2022-02-09 11:32:11 +0530

    update api url

--- packages/card-quiz/src/App.js
+++ packages/card-quiz/src/App.js
@@ -4,6 +4,11 @@ import Card from "./Card.js";
 const data = [
   { id: 1, statement: "Mahavir bhagwan ka chinha kya hai ?" },
   { id: 2, statement: "Chandraprabhu bhagwan ka chinha kya hai ?" },
+  { id: 3, statement: "Aadinath bhagwan kaha se moksh gae" },
+  { id: 4, statement: "Vasupujva bhagwan kaha se moksh gae" },
+  { id: 5, statement: "shikhar ji se kitne tirthankar moksh gae ?" },
+  { id: 6, statement: "Mahavir bhagwan kaha se moksh gae ?" },
+  { id: 7, statement: "Neminath bhagwan kaha se moksh gae ?" },
 ];
 
 function App() {
